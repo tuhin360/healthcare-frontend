66a6d9d8154122b6bf2c791c66ba04c53f447086
tuhin <tuhin153695@gmail.com>
2025-03-01 17:26:13 +0600

    bug fixes using dynamic import lazy loading

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,17 +1,10 @@
 "use client";
-import { getUserInfo, removeUser } from "@/services/auth.services";
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import dynamic from "next/dynamic";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 const Navbar = () => {
-  const userInfo = getUserInfo();
-  const router = useRouter();
-  const handleLogOut = () => {
-    removeUser();
-    router.refresh();
-  };
-
+  const AuthButton = dynamic(() => import('@/components/UI/AuthButton/AuthButton'), { ssr: false })
   return (
     <Container>
       <Stack
@@ -38,15 +31,7 @@ const Navbar = () => {
           <Typography>Diagnostics</Typography>
           <Typography>NGOs</Typography>
         </Stack>
-        {userInfo?.userId ? (
-          <Button color="error" onClick={handleLogOut}>
-            Logout
-          </Button>
-        ) : (
-          <Button component={Link} href="/login">
-            Login
-          </Button>
-        )}
+        <AuthButton />
       </Stack>
     </Container>
   );
